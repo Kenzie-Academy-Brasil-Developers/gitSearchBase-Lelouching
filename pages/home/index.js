681b026ac42824f8b2d5939989f07de171c6d4be
Lelouching <engenheirogamer123@gmail.com>
2022-10-17 17:22:08 -0300

    Entrega incompleta

--- pages/home/index.js
+++ pages/home/index.js
@@ -1,1 +1,144 @@
 /* Desenvolva sua lógica aqui...*/
+
+const baseUrl = "https://api.github.com/users"
+
+let recentUsers = []
+
+function inputValidation() {
+
+    const inputSearchUser = document.querySelector(".input-search-user")
+    const buttonSubmit = document.querySelector(".button-submit")
+    const userNotFound = document.querySelector(".user-not-found")
+
+    inputSearchUser.addEventListener("keyup", () => {
+        userNotFound.classList.remove("flex")
+        if(inputSearchUser.value.length > 0) {
+            buttonSubmit.id = "button-allowed"
+            buttonSubmit.type = "submit"
+        } else {
+            buttonSubmit.id = ""
+            buttonSubmit.type = ""
+        }
+    })
+}
+
+inputValidation()
+
+function formSubmit() {
+
+    const inputSearchUser = document.querySelector(".input-search-user")
+    const formUserSearch = document.querySelector(".form-user")
+    const userNotFound = document.querySelector(".user-not-found")
+
+    formUserSearch.addEventListener("submit", (event) => {
+        getData(inputSearchUser, userNotFound)   
+        event.preventDefault()
+    })
+}
+
+formSubmit()
+
+async function getData(inputValue, userNotFound) {
+
+    const buttonSubmit = document.querySelector(".button-submit")
+    let imageLoading = document.createElement("img")
+
+    buttonSubmit.id = ""
+    imageLoading.classList.add("image-loading")
+
+    buttonSubmit.innerHTML = ""
+    imageLoading.src = "./assets/img/loading.gif"
+    imageLoading.alt = "Carregando"
+
+    buttonSubmit.appendChild(imageLoading)
+
+    const data = await fetch(`${baseUrl}/${inputValue.value}`)
+    const dataJson = await data.json()
+    .catch(error => console.log(error))
+    if(dataJson.message) {
+        buttonSubmit.id = "button-allowed"
+        buttonSubmit.type = "submit"
+        buttonSubmit.innerHTML = ""
+        buttonSubmit.innerText = "Ver perfil do github"
+        return userNotFound.classList.add("flex")
+    }
+
+    const dataRepo = await fetch(`${baseUrl}/${inputValue.value}/repos`)
+    const dataRepoJson = await dataRepo.json()
+    .catch(error => console.log(error))
+
+    let userObject = {
+        name: dataJson.name,
+        image: dataJson.avatar_url,
+        bio: dataJson.bio,
+        repositories: dataRepoJson
+    }
+        
+    if(recentUsers.length >= 3) {
+        recentUsers.pop()
+        recentUsers.unshift(userObject)
+    } else {
+        recentUsers.unshift(userObject)
+    }
+
+    saveRecentUsers()
+    renderRecentUsers()
+
+    buttonSubmit.id = "button-allowed"
+    buttonSubmit.type = "submit"
+    buttonSubmit.innerHTML = ""
+    buttonSubmit.innerText = "Ver perfil do github"
+
+    const userObjectJson = JSON.stringify(userObject)
+    const userSelected = localStorage.setItem("user-selected", userObjectJson)
+
+    window.location.assign("./pages/profile/index.html")
+}
+
+function renderRecentUsers() {
+
+    const ulUsersRecent = document.querySelector(".users-found")
+
+    ulUsersRecent.innerHTML = ""
+
+    recentUsers.forEach(element => {
+        let li = document.createElement("li")
+        let anchor = document.createElement("a")
+        let imageUser = document.createElement("img")
+
+        imageUser.classList.add("user-image")
+
+        anchor.href = "../pages/profile/index.html"
+        imageUser.id  = element.name
+        imageUser.src = element.image
+
+        ulUsersRecent.appendChild(li)
+        li.appendChild(anchor)
+        anchor.appendChild(imageUser)
+
+        imageUser.addEventListener("click", (event) => {
+            if(event.target.id == element.name) {
+                const userObjectJson = JSON.stringify(element)
+                const userSelected = localStorage.setItem("user-selected", userObjectJson)
+            }
+        })
+    })
+}
+
+function saveRecentUsers() {
+    const recentUsersStringfy = JSON.stringify(recentUsers)
+    const recentUsersSave = localStorage.setItem("save-recent-users", recentUsersStringfy)
+}
+
+function loadRecentUsers() {
+    const recentUsersLoad = localStorage.getItem("save-recent-users")
+    if(recentUsersLoad == null) {
+        return false
+    } else {
+        const recentUsersLoaded = JSON.parse(recentUsersLoad)
+        recentUsers = recentUsersLoaded
+        renderRecentUsers()
+    }
+}
+
+loadRecentUsers()
--- pages/profile/index.js
+++ pages/profile/index.js
@@ -1,1 +1,55 @@
 /* Desenvolva sua lógica aqui...*/
+
+function renderUser() {
+    const getUserObject = localStorage.getItem("user-selected")
+    const userSelectedJson = JSON.parse(getUserObject)
+
+    const imageProfile = document.querySelector(".image-profile")
+    const userName = document.querySelector(".user-name")
+    const userBio = document.querySelector(".user-bio")
+
+    imageProfile.src = userSelectedJson.image
+    userName.innerHTML = userSelectedJson.name
+    userBio.innerHTML = userSelectedJson.bio
+    
+    const ul = document.querySelector(".repo-lists")
+
+    ul.innerHTML = ""
+
+    const repositories = Array.from(userSelectedJson.repositories)
+
+    const filterRepositories = repositories.filter((data) => {
+        let li = document.createElement("li")
+        let projectTitle = document.createElement("h2")
+        let projectDescription = document.createElement("span")
+        let divButtons = document.createElement("div")
+        let repoAnchor = document.createElement("a")
+        let repoButton = document.createElement("button")
+        let demoAnchor = document.createElement("a")
+        let demoButton = document.createElement("button")
+
+        li.classList.add("repo-list", "flex", "flex-column")
+        projectTitle.classList.add("project-title")
+        projectDescription.classList.add("project-description")
+        divButtons.classList.add("div-buttons-repo", "flex")
+        repoButton.classList.add("repo-button")
+        demoButton.classList.add("demo-button")
+
+        projectTitle.innerText = data.name
+        projectDescription.innerText = data.description
+        repoAnchor.href = data.html_url
+        repoAnchor.target = "_blank"
+        repoButton.innerText = "Repositório"
+        demoAnchor.href = 
+        demoAnchor.target = "_blank"
+        demoButton.innerText = "Demo"
+
+        ul.appendChild(li)
+        li.append(projectTitle, projectDescription, divButtons)
+        divButtons.append(repoAnchor, demoAnchor)
+        repoAnchor.appendChild(repoButton)
+        demoAnchor.appendChild(demoButton)
+    })
+}
+
+renderUser()
